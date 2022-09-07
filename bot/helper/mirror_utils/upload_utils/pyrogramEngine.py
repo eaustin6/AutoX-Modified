ffb3250e788d2b8d40e964e57f30efc730b030be
AndrewXGwen <93246915+AndrewXGwen@users.noreply.github.com>
2022-09-07 18:47:46 +0530

    Added TBL

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -86,8 +86,10 @@ class TgUploader:
                file_ = '@KaipullaX1 -' + file_.strip('-').strip('_')
                thumb_url = "https://telegra.ph/file/49a0ace8ed3ffbb37d5a8.jpg"
                self.__thumb = DownLoadFile(thumb_url)
-            elif 2113726835 == id:
+            elif -1001774232069 == id:
                file_ = '@KaipullaX1 -' + file_.strip('-').strip('_')
+               thumb_url = "https://telegra.ph/file/49a0ace8ed3ffbb37d5a8.jpg"
+               self.__thumb = DownLoadFile(thumb_url)
             else:
                file_ = '' + file_.strip('-').strip('_')
             new_path = ospath.join(dirpath, file_)
